10da767a118ccff15d19f8dfb986ec89650e77cc
Chandler Eyre <chabyeyre@gmail.com>
2023-10-16 17:44:36 -0600

    Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,30 @@
 import "./styles/main.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { React, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 
 function App() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(true);
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" index element={<Home />} />
+          <Route
+            path="/"
+            element={
+              isUserAuthenticated ? (
+                <Navigate to="/home" replace />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
-      <Home></Home>
     </div>
   );
 }
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,8 +2,31 @@ import React from "react";
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-wrapper">
+      <div className="login-modal">
+        <h1>Log in</h1>
+        <input
+          type="text"
+          placeholder="username"
+          className="login-input"
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          className="login-input"
+        ></input>
+        <button type="submit" className="login-button">
+          Log in
+        </button>
+        <div className="divider">
+          <span className="line"></span>
+          <p>OR</p>
+          <span className="line"></span>
+        </div>
+        <a className="link" href="#">
+          Sign up
+        </a>
+      </div>
     </div>
   );
 }
